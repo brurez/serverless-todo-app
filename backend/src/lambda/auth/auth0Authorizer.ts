0ccf641315ee13c90e3f8b3f0e3fff1eed058536
Bruno de Rezende <brunoderezende@hotmail.com>
2020-04-07 16:28:43 -0300

    Fixes backend todos

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,25 +1,23 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
-import 'source-map-support/register';
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken';
-import { createLogger } from '../../utils/logger';
-import Axios from 'axios';
-import { Jwt } from '../../auth/Jwt';
-import { JwtPayload } from '../../auth/JwtPayload';
+import { verify, decode } from 'jsonwebtoken'
+import { createLogger } from '../../utils/logger'
+import Axios from 'axios'
+import { Jwt } from '../../auth/Jwt'
+import { JwtPayload } from '../../auth/JwtPayload'
 
-const logger = createLogger('auth');
+const logger = createLogger('auth')
 
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://brurez.auth0.com/.well-known/jwks.json';
+const jwksUrl = 'https://brurez.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken);
+  logger.info('Authorizing a user', event.authorizationToken)
   try {
-    const jwtToken = await verifyToken(event.authorizationToken);
-    logger.info('User was authorized', jwtToken);
+    const jwtToken = await  verifyToken(event.authorizationToken)
+    logger.info('User was authorized', jwtToken)
 
     return {
       principalId: jwtToken.sub,
@@ -33,9 +31,9 @@ export const handler = async (
           }
         ]
       }
-    };
+    }
   } catch (e) {
-    logger.error('User not authorized', { error: e.message });
+    logger.error('User not authorized', { error: e })
 
     return {
       principalId: 'user',
@@ -49,9 +47,9 @@ export const handler = async (
           }
         ]
       }
-    };
+    }
   }
-};
+}
 
  async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
@@ -75,13 +73,13 @@ export const handler = async (
 }
 
 function getToken(authHeader: string): string {
-  if (!authHeader) throw new Error('No authentication header');
+  if (!authHeader) throw new Error('No authentication header')
 
   if (!authHeader.toLowerCase().startsWith('bearer '))
-    throw new Error('Invalid authentication header');
+    throw new Error('Invalid authentication header')
 
-  const split = authHeader.split(' ');
-  const token = split[1];
+  const split = authHeader.split(' ')
+  const token = split[1]
 
-  return token;
+  return token
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,26 +1,25 @@
 import 'source-map-support/register'
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  APIGatewayProxyHandler
-} from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserId} from '../../helpers/authHelper'
+import { TodosAccess } from '../../dataLayer/todosAccess'
+import { S3Helper } from '../../helpers/s3Helper'
+import { ApiResponseHelper } from '../../helpers/apiResponseHelper'
+import { createLogger } from '../../utils/logger'
 
-import TodoRepository from '../../repositories/TodosRepository'
-import {getUserId} from "../utils";
+const s3Helper = new S3Helper()
+const apiResponseHelper= new ApiResponseHelper()
+const logger = createLogger('todos')
 
-export const handler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-  const userId = getUserId(event);
-  const items = TodoRepository.getPerUser(userId);
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
+    const authHeader = event.headers['Authorization']
+    const userId = getUserId(authHeader) 
+    logger.info(`get groups for user ${userId}`)
+    const result = await new TodosAccess().getUserTodos(userId)
+      
+    for(const record of result){
+        record.attachmentUrl = await s3Helper.getTodoAttachmentUrl(record.todoId)
+    }
 
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      items
-    })
-  }
-};
+    return apiResponseHelper.generateDataSuccessResponse(200,'items',result)
+}
